f8267bdfae7234ed09c10cdec118455c69265a40
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-05 14:20:17 +0100

    Added delegate for get and delete method

--- BookManagementSystem/DelegateClass.cs
+++ BookManagementSystem/DelegateClass.cs
@@ -5,8 +5,10 @@ namespace BookManagementSystem
     public class DelegateClass
     {
         List<Book> books = new();
-        public delegate void BookService(Book book);
-
+        public delegate void BookService(Book book); // Create and update
+        public delegate void Service(Guid id); // Get and delete
         
+
+
     }
 }
