09d01bf8e7757def2796dd79921bf14c14150246
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-05 14:28:19 +0100

    created event handlers

---
+++ BookManagementSystem/EventClass.cs
@@ -0,0 +1,20 @@
+﻿using static BookManagementSystem.DelegateClass;
+
+namespace BookManagementSystem
+{
+    public  class EventClass
+    {
+        public event BookNotification BookEvent;
+        
+        public void StartProcess(string bookName)
+        {
+            Console.WriteLine("Starting Operation");
+            OnStart(bookName);
+        }
+        
+        protected virtual void OnStart(string bookName)
+        {
+            BookEvent?.Invoke(this, bookName);
+        }
+    }
+}
---
+++ BookManagementSystem/EventHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManagementSystem
+{
+    public class EventHandler
+    {
+        public static void AddBook(object obj,string name)
+        {
+            Console.WriteLine($"{name} book added successfully");
+        }
+        
+        public static void UpdateBook(object obj, string name)
+        {
+            Console.WriteLine($"{name} book updated successfully");
+        }
+        
+        public static void DeleteBook(object obj, string name)
+        {
+            Console.WriteLine($"{name} book deleted successfully");
+        }
+
+        /*        public static void GetExistingBook(object obj, string name)
+        {
+            Console.WriteLine($"{name} book Added");
+        }*/
+    }
+}
