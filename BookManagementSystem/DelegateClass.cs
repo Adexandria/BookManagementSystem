092d9287bcb0aa9bb4fd441ecb3df84bd2fac888
Suzee27 <suzieoborokumo@gmail.com>
2022-08-05 22:47:51 +0100

    Added a GetBook method to retrieve a book by Id

--- BookManagementSystem/DelegateClass.cs
+++ BookManagementSystem/DelegateClass.cs
@@ -6,10 +6,28 @@ namespace BookManagementSystem
     {
         List<Book> books = new();
         public delegate void BookService(Book book); // Create and update
-        public delegate void Service(Guid id); // Get and delete
-        
+        public delegate void DeleteBookService(int id);  // delete
+        public delegate Book GetBookService(int id);    // Get
+
         public delegate void BookNotification(object obj,string name);
 
+        public void DeleteBook(int id)
+        {
+            var _book = books.FirstOrDefault(x => x.BookId == id);
+            if (_book != null)
+            {
+                books.Remove(_book);
+            }
+        }
 
+        public Book GetBook(int id)
+        {
+            var _book = books.FirstOrDefault(x => x.BookId == id);
+            if (_book != null)
+            {
+                return _book;
+            }
+            else return null;
+        }
     }
 }
