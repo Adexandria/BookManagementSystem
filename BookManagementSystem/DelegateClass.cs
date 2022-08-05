4541cc66b957fc0f3a3a3d7f8ea1abb4acdee9c2
Aderibigbe Adeola <adeolaaderibigbe09@gmail.com>
2022-08-05 13:23:21 +0100

    Add project files.

---
+++ BookManagementSystem/DelegateClass.cs
@@ -0,0 +1,12 @@
+﻿using BookManagementSystem.Model;
+
+namespace BookManagementSystem
+{
+    public class DelegateClass
+    {
+        List<Book> books = new();
+        public delegate void BookService(Book book);
+
+        
+    }
+}
