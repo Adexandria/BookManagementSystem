95b620d9db87ff86df6da52749eb9ee65c2c1cf0
Samuel Jaja <samuel.jaja21@gmail.com>
2022-08-06 10:18:20 +0100

    Made changes to DelegateClass and EventClass

--- BookManagementSystem/DelegateClass.cs
+++ BookManagementSystem/DelegateClass.cs
@@ -10,40 +10,63 @@ namespace BookManagementSystem
         
         public delegate void BookNotification(object obj,string name);
 
+        static EventClass createEvent = new();
+        static EventClass updateEvent = new();
+        static EventClass deleteEvent = new();
+        static EventClass getEvent = new();
+        static EventHandler eventHandler = new();
+
+        
+
         public void CreateBook(Book book)
         {
+            createEvent.BookEvent += eventHandler.AddBook;
+            createEvent.StartProcess(book.Name);
             var checkBook = books.FirstOrDefault(x => x.Equals(book));
             if(checkBook is null)
             {
                 books.Add(book);
             }
+            createEvent.EndProcess(book.Name);
+            
         }
 
         public void UpdateBookDescription(Book book)
+
         {
+            updateEvent.BookEvent += eventHandler.UpdateBookDescription;
+            updateEvent.StartProcess(book.Name);
             var findBook = books.FirstOrDefault(x => x.BookId == book.BookId);
             if (findBook != null)
             {
                 findBook.Description = book.Description;
             }
+            updateEvent.EndProcess(book.Name);
+
         }
 
         public void UpdateBookAuthor(Book book)
         {
+            updateEvent.BookEvent += eventHandler.UpdateBookAuthor;
+            updateEvent.StartProcess(book.Name);
             var findBook = books.FirstOrDefault(x => x.BookId == book.BookId);
             if (findBook != null)
             {
-                findBook.Description = book.Author;
+                findBook.Author = book.Author;
             }
+            updateEvent.EndProcess(book.Name);
         }
 
         public void UpdateBookName(Book book)
         {
+            updateEvent.BookEvent += eventHandler.UpdateBookName;
+            updateEvent.StartProcess(book.Name);
             var findBook = books.FirstOrDefault(x => x.BookId == book.BookId);
             if (findBook != null)
             {
-                findBook.Description = book.Name;
+                findBook.Name = book.Name;
             }
+            updateEvent.EndProcess(book.Name);
         }
     }
 }
