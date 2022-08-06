c6a778130e0e47d3f4863fc1b0186f918bec5ff3
Suzee27 <suzieoborokumo@gmail.com>
2022-08-06 10:32:55 +0100

    Added Deletebook method and GetBook method

--- BookManagementSystem/DelegateClass.cs
+++ BookManagementSystem/DelegateClass.cs
@@ -4,14 +4,14 @@ namespace BookManagementSystem
 {
     public class DelegateClass
     {
-        List<Book> books = new();
+        static List<Book> books = new();
+
         public delegate void BookService(Book book); // Create and update
-        public delegate void DeleteBookService(int id);  // delete
-        public delegate Book GetBookService(int id);    // Get
+        public delegate void Service(int id);  // Get and delete    
 
         public delegate void BookNotification(object obj,string name);
 
-        public void DeleteBook(int id)
+        public static void DeleteBook(int id)
         {
             var _book = books.FirstOrDefault(x => x.BookId == id);
             if (_book != null)
@@ -20,14 +20,15 @@ namespace BookManagementSystem
             }
         }
 
-        public Book GetBook(int id)
+        public static void GetBook(int id)
         {
             var _book = books.FirstOrDefault(x => x.BookId == id);
             if (_book != null)
             {
-                return _book;
+                Console.WriteLine(_book);
             }
-            else return null;
+            else Console.WriteLine("Book not found");
         }
+        
     }
 }
